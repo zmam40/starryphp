064e7999f2646e72033f66b376e9d745b82685c2
zmam40 <mmontgomery@pcch.org>
2012-11-28 15:43:22 -0500

    test

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
-<?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  return "dbname=dcgqqjhfkugsvc host=ec2-107-22-168-220.compute-1.amazonaws.com port=5432 user=umcvabalfiwehs password=9K08LaWTfzzygvWNqjmDb05gn1 sslmode=require";
-}
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-   echo "Database connection error."
-   exit;
-}
-$result = pg_query($db, "SELECT statement goes here");
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Refresh" content="0; URL=http://blog.pcch.org/support" />
+<title>blog</title>
+</head>
+
+<body>
+</body>
+</html>
